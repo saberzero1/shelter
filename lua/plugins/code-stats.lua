f865796717a2c5eac4533848f0ca6b64cd228dfa
saberzero1 <github@emilebangma.com>
2024-06-25 14:56:40 +0200

    Retrieve Code::Stats from ENV

--- lua/plugins/code-stats.lua
+++ lua/plugins/code-stats.lua
@@ -4,7 +4,7 @@
 local code_stats = require("codestats-nvim")
 
 code_stats.setup({
-    token = nil, -- When the token is not provided, codestats.nvim will fallback to the CODESTATS_API_KEY environment variable
+    -- token = nil, -- When the token is not provided, codestats.nvim will fallback to the CODESTATS_API_KEY environment variable
     endpoint = "https://codestats.net", -- The endpoint which should be used. Mostly you dont want to overwrite it
     interval = 60 -- The interval in seconds between pushes to the CodeStats API
 })
