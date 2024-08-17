1a9936325c7aa1305cee0063024ffdbcbdcd549b
saberzero1 <github@emilebangma.com>
2024-08-17 21:02:26 +0200

    Semicolon

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -6,14 +6,14 @@ return { -- Useful plugin to show you pending keybinds.
     spec = {
         -- Document existing key chains
         {
-           mode = { "n", "v" }
+          mode = { "n", "v" },
           { "<leader>c", group = "[C]ode" },
           { "<leader>d", group = "[D]ocument" },
           { "<leader>r", group = "[R]ename" },
           { "<leader>s", group = "[S]earch" },
           { "<leader>w", group = "[W]orkspace" },
           { "<leader>t", group = "[T]oggle" },
-          { "<leader>h", group = "Git [H]unk",  },
+          { "<leader>h", group = "Git [H]unk"  },
         },
       },
     },
