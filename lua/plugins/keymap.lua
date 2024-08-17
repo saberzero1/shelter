f64a5f7f1f361aeeda34c64b60f7996b7d7278a8
saberzero1 <github@emilebangma.com>
2024-08-17 20:55:52 +0200

    V3 rewrite

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -2,18 +2,23 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').add({
-      { '<leader>c', group = '[C]ode' },
-      { '<leader>d', group = '[D]ocument' },
-      { '<leader>r', group = '[R]ename' },
-      { '<leader>s', group = '[S]earch' },
-      { '<leader>w', group = '[W]orkspace' },
-      { '<leader>t', group = '[T]oggle' },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-    })
+  opts = {
+    spec = {
+        -- Document existing key chains
+        {
+           mode = { "n", "v" }
+          { "<leader>c", group = "[C]ode" },
+          { "<leader>d", group = "[D]ocument" },
+          { "<leader>r", group = "[R]ename" },
+          { "<leader>s", group = "[S]earch" },
+          { "<leader>w", group = "[W]orkspace" },
+          { "<leader>t", group = "[T]oggle" },
+          { "<leader>h", group = "Git [H]unk",  },
+        },
+      },
+    },
+  config = function(_, opts) -- This is the function that runs, AFTER loading
+    local wk = require("which-key")
+    wk.setup(opts)
   end,
 }
