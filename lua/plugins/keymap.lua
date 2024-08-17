20508f31634180927f0b6decaf16dc63666b6db8
saberzero1 <github@emilebangma.com>
2024-08-17 20:10:51 +0200

    Local binding

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -3,10 +3,12 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+    local whichkey = require 'which-key'
+
+    whichkey.setup()
 
     -- Document existing key chains
-    require('which-key').add({
+    whichkey.add({
       { '<leader>c', group = '[C]ode' },
       { '<leader>d', group = '[D]ocument' },
       { '<leader>r', group = '[R]ename' },
