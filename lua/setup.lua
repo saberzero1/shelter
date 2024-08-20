f64482a5e116b406ed6aa1e76118487e0dcd579e
saberzero1 <github@emilebangma.com>
2024-08-20 23:26:05 +0200

    Azure Data Studio setup

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,65 +1,118 @@
-require "config.options"
+require 'config.options'
 
-require "plugins.neovide"
+require 'plugins.neovide'
 
-require("lazy").setup({
-  { import = "plugins.copilot" },
-  { import = "plugins.vscode" },
-  --{ import = "plugins.neovide" },
-  { import = "plugins.bad-practices" },
-  { import = "plugins.git" },
-  { import = "plugins.keymap" },
-  { import = "plugins.fuzzy-finder" },
-  { import = "plugins.lsp" },
-  { import = "plugins.luvit" },
-  { import = "plugins.lsp-config" },
-  { import = "plugins.autoformat" },
-  { import = "plugins.autocompletion" },
-  { import = "plugins.theme" },
-  { import = "plugins.comments" },
-  { import = "plugins.mini" },
-  { import = "plugins.treesitter" },
-} --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    backdrop = 100,
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
+if not vim.g.vscode then
+  require('lazy').setup({
+    { import = 'plugins.copilot' },
+    --{ import = "plugins.vscode" },
+    --{ import = "plugins.neovide" },
+    { import = 'plugins.bad-practices' },
+    { import = 'plugins.git' },
+    { import = 'plugins.keymap' },
+    { import = 'plugins.fuzzy-finder' },
+    { import = 'plugins.lsp' },
+    { import = 'plugins.luvit' },
+    { import = 'plugins.lsp-config' },
+    { import = 'plugins.autoformat' },
+    { import = 'plugins.autocompletion' },
+    { import = 'plugins.theme' },
+    { import = 'plugins.comments' },
+    { import = 'plugins.mini' },
+    { import = 'plugins.treesitter' },
+  } --[[@as LazySpec]], {
+    -- Configure any other `lazy.nvim` configuration options here
+    ui = {
+      -- If you are using a Nerd Font: set icons to an empty table which will use the
+      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+      backdrop = 100,
+      icons = vim.g.have_nerd_font and {} or {
+        cmd = '⌘',
+        config = '🛠',
+        event = '📅',
+        ft = '📂',
+        init = '⚙',
+        keys = '🗝',
+        plugin = '🔌',
+        runtime = '💻',
+        require = '🌙',
+        source = '📄',
+        start = '🚀',
+        task = '📌',
+        lazy = '💤 ',
       },
     },
-  },
-} --[[@as LazyConfig]])
+    performance = {
+      rtp = {
+        -- disable some rtp plugins, add more to your liking
+        disabled_plugins = {
+          'gzip',
+          'netrwPlugin',
+          'tarPlugin',
+          'tohtml',
+          'zipPlugin',
+        },
+      },
+    },
+  } --[[@as LazyConfig]])
+else
+  require('lazy').setup({
+    --{ import = "plugins.copilot" },
+    { import = 'plugins.vscode' },
+    --{ import = "plugins.neovide" },
+    { import = 'plugins.bad-practices' },
+    { import = 'plugins.git' },
+    { import = 'plugins.keymap' },
+    { import = 'plugins.fuzzy-finder' },
+    { import = 'plugins.lsp' },
+    { import = 'plugins.luvit' },
+    { import = 'plugins.lsp-config' },
+    { import = 'plugins.autoformat' },
+    { import = 'plugins.autocompletion' },
+    { import = 'plugins.theme' },
+    { import = 'plugins.comments' },
+    { import = 'plugins.mini' },
+    { import = 'plugins.treesitter' },
+  } --[[@as LazySpec]], {
+    -- Configure any other `lazy.nvim` configuration options here
+    ui = {
+      -- If you are using a Nerd Font: set icons to an empty table which will use the
+      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+      backdrop = 100,
+      icons = vim.g.have_nerd_font and {} or {
+        cmd = '⌘',
+        config = '🛠',
+        event = '📅',
+        ft = '📂',
+        init = '⚙',
+        keys = '🗝',
+        plugin = '🔌',
+        runtime = '💻',
+        require = '🌙',
+        source = '📄',
+        start = '🚀',
+        task = '📌',
+        lazy = '💤 ',
+      },
+    },
+    performance = {
+      rtp = {
+        -- disable some rtp plugins, add more to your liking
+        disabled_plugins = {
+          'gzip',
+          'netrwPlugin',
+          'tarPlugin',
+          'tohtml',
+          'zipPlugin',
+        },
+      },
+    },
+  } --[[@as LazyConfig]])
 
-if vim.g.vscode then
-  require "plugins.azure-data-studio"
+  require 'plugins.azure-data-studio'
 end
 
-require "config.keymaps"
-require "config.autocmds"
+require 'config.keymaps'
+require 'config.autocmds'
 
-require "overrides"
+require 'overrides'
