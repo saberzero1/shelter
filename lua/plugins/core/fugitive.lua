681cb3ed02281a0e69b65ed76ff7a373d32c4fa3
saberzero1 <github@emilebangma.com>
2024-09-05 09:40:09 +0200

    Git status

--- lua/plugins/core/fugitive.lua
+++ lua/plugins/core/fugitive.lua
@@ -1,4 +1,7 @@
 ---@type LazySpec
 return {
   'tpope/vim-fugitive',
+  keys = {
+    { "<leader>gs", '<cmd>G status<CR>', desc = 'Git Status' },
+  },
 }
--- lua/plugins/core/which-key.lua
+++ lua/plugins/core/which-key.lua
@@ -48,7 +48,7 @@ return { -- Useful plugin to show you pending keybinds.
       { '<leader>s', group = '[S]earch' },
       { '<leader>w', group = '[W]orkspace' },
       { '<leader>t', group = '[T]oggle' },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { '<leader>g', group = '[G]it Hunk', mode = { 'n', 'v' } },
       { '<leader>h', group = '[H]arpoon' },
     },
   },
