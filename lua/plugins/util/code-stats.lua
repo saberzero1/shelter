f1d18835f42f43de4778805fcb88dab1889bf54b
saberzero1 <github@emilebangma.com>
2024-09-09 17:11:12 +0200

    Code::Stats

---
+++ lua/plugins/util/code-stats.lua
@@ -0,0 +1,16 @@
+-- From https://gitlab.com/code-stats/code-stats-vim
+
+---@type LazySpec
+return {
+  'code-stats/code-stats-vim',
+  url = 'https://gitlab.com/code-stats/code-stats-vim.git',
+  config = function()
+    local api_key = vim.api.nvim_exec('! printenv CODESTATS_API_KEY', true)
+    if api_key == nil or api_key == '' then
+      vim.g.codestats_error = 'No CodeStats API key found'
+    else
+      vim.g.codestats_api_key = api_key
+      vim.g.codestats_api_url = 'https://codestats.net/'
+    end
+  end
+}
