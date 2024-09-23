b0e9bbed01b0f3be1acee3b0973a6b5d42710533
saberzero1 <github@emilebangma.com>
2024-09-23 22:54:11 +0200

    NeoCord

---
+++ lua/plugins/integration/discord.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+  'IogaMaster/neocord',
+  event = 'VeryLazy',
+  opts = {
+    logo = 'https://raw.githubusercontent.com/saberzero1/shelter/refs/heads/master/doc/neovim-logo.png',
+  },
+}
--- lua/plugins/integration/init.lua
+++ lua/plugins/integration/init.lua
@@ -1,3 +1,4 @@
 return {
+  { import = 'plugins.integration.discord' },
   { import = 'plugins.integration.neovide' },
 }
