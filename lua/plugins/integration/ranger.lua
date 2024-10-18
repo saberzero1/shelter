ea719c54355b75aedd844bf3be9dc04e901b5270
saberzero1 <github@emilebangma.com>
2024-10-19 01:12:56 +0200

    Added initial Ranger support

---
+++ lua/plugins/integration/ranger.lua
@@ -0,0 +1,17 @@
+-- https://github.com/kevinhwang91/rnvimr
+-- https://github.com/VardanHeroic/ranger.nvim
+
+---@type LazySpec
+return {
+  "kelly-lin/ranger.nvim",
+  config = function()
+    require("ranger-nvim").setup({ replace_netrw = true })
+    vim.api.nvim_set_keymap("n", "<leader>o", "", {
+      desc = "[o]pen Ranger",
+      noremap = true,
+      callback = function()
+        require("ranger-nvim").open(true)
+      end,
+    })
+  end,
+}
