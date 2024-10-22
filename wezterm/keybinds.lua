38c36f8717311b5fccb27e344281938593b4b408
saberzero1 <github@emilebangma.com>
2024-10-22 17:27:48 +0200

    Wezterm Meh and Super keys

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
 
+--[[
 local keys = {
 	{
 		key = "g",
@@ -150,11 +151,177 @@ local keys = {
 		end),
 	},
 }
+]]--
+local keys = {
+	{
+		key = "g",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action_callback(function(window, pane)
+			local current_tab_id = pane:tab():tab_id()
+			local cmd = "lazygit ; wezterm cli activate-tab --tab-id " .. current_tab_id .. " ; exit\n"
+			local tab, tab_pane, _ = window:mux_window():spawn_tab({})
+			tab_pane:send_text(cmd)
+			tab:set_title(wezterm.nerdfonts.dev_git .. " Lazygit")
+		end),
+	},
+	{
+		key = "-",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 30 },
+		}),
+	},
+	{
+		key = "_",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 25 },
+		}),
+	},
+	{
+		key = "z",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "o",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.ActivateLastTab,
+	},
+	{
+		key = "t",
+		mods = "CTRL | SHIFT | ALT",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "w",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "p",
+		mods = "SUPER",
+		action = act.PaneSelect({}),
+	},
+	{
+		key = "p",
+		mods = "CTRL | SHIFT | ALT",
+		action = act.PaneSelect({
+			mode = "SwapWithActive",
+		}),
+	},
+	{
+		key = "d",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action_callback(function(window, pane)
+			window:perform_action(
+				act.SwitchToWorkspace({
+					name = ".dotfiles",
+					spawn = { cwd = wezterm.home_dir .. "/Documents/Repos/dotfiles-submodules/shelter" },
+				}),
+				pane
+			)
+			window:set_right_status(window:active_workspace())
+		end),
+	},
+	{
+		key = "e",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.CharSelect({
+			copy_on_select = true,
+			copy_to = "ClipboardAndPrimarySelection",
+		}),
+	},
+	{
+		key = "b",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.Multiple({
+			wezterm.action_callback(function(win, pane)
+				-- wezterm.mux.write_session_state("/tmp/wezterm/session_state.json")
+				local workspace_switcher = wezterm.plugin.require_as_alias(
+					"https://github.com/MLFlexer/smart_workspace_switcher.wezterm/",
+					"some_alias"
+				)
 
+				wezterm.log_info(workspace_switcher.a)
+				-- local ws = require("some_alias.plugin.init")
+				-- wezterm.log_info(ws)
+				wezterm.log_info(require("some_alias"))
+			end),
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.Multiple({
+			wezterm.action_callback(function(win, pane)
+				wezterm.reload_configuration()
+			end),
+		}),
+	},
+	{
+		key = "m",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action.Multiple({
+			wezterm.action_callback(function(win, pane)
+				local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+				resurrect.fuzzy_load(win, pane, function(id, label)
+					id = string.match(id, "([^/]+)$")
+					id = string.match(id, "(.+)%..+$")
+					local state = resurrect.load_state(id, "workspace")
+					local workspace_state = resurrect.workspace_state
+					workspace_state.restore_workspace(state, {
+						relative = true,
+						restore_text = true,
+						on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+					})
+				end)
+			end),
+		}),
+	},
+	{ key = "L", mods = "SUPER", action = wezterm.action.ShowDebugOverlay },
+
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollToPrompt(-1) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
+	{
+		key = "n",
+		mods = "CTRL | SHIFT | ALT",
+		action = wezterm.action_callback(function(win, pane)
+			local tab, window = pane:move_to_new_tab()
+		end),
+	},
+	{
+		key = "N",
+		mods = "SUPER",
+		action = wezterm.action_callback(function(win, pane)
+			local tab, window = pane:move_to_new_window()
+		end),
+	},
+}
+
+local function tab_switch_keys(key_table, modifier)
+  key_list = { "!", "@", "#", "$", "%", "^", "&", "*", "(" }
+	for i = 1, 9 do
+		table.insert(key_table, {
+			key = key_list[i],
+			mods = modifier,
+			action = act.ActivateTab(i - 1),
+		})
+	end
+	table.insert(key_table, {
+		key = ")",
+		mods = modifier,
+		action = act.ActivateTab(9),
+	})
+end
+
+--[[
 local function tab_switch_keys(key_table, modifier)
 	for i = 1, 9 do
 		table.insert(key_table, {
-			key = tostring(i),
+			key = toString(i),
 			mods = modifier,
 			action = act.ActivateTab(i - 1),
 		})
@@ -165,7 +332,9 @@ local function tab_switch_keys(key_table, modifier)
 		action = act.ActivateTab(9),
 	})
 end
+]]--
 
-tab_switch_keys(keys, "ALT")
+--tab_switch_keys(keys, "ALT")
+tab_switch_keys(keys, "CTRL | SHIFT | ALT")
 
 return keys
