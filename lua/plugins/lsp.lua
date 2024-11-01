67b12c27ec8f1c5fe4d868189de146989e8ba005
saberzero1 <github@emilebangma.com>
2024-11-02 00:36:45 +0100

    LSP

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      -- pyright will be automatically installed with mason and loaded with lspconfig
+      pyright = {},
+      ts_ls = {},
+    },
+  },
+}
