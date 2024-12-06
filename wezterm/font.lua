ef6357f27b2c47b18286ae6c10a1f56389c26d97
saberzero1 <github@emilebangma.com>
2024-12-06 19:33:28 +0100

    Updated WezTerm Fonts

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -1,26 +1,17 @@
 local font = require('wezterm').font
+local font_with_fallback = require('wezterm').font_with_fallback
 local harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' }
 
-local function font_with_fallback(linux_families, darwin_families)
-  local result = linux_families
-  if require('wezterm').target_triple:find 'darwin' ~= nil then
-    result = darwin_families or linux_families
+local function os_specific_font(families)
+  if require('wezterm').target_triple:find 'darwin' ~= nil and families[2] ~= nil then
+    return font_with_fallback { families[2] }
   end
-  return font(result)
+  return font_with_fallback { families[1] }
 end
 
 return {
   -- default font
-  font = font_with_fallback {
-    {
-      family = 'MonaspiceNe Nerd Font',
-      -- family = "MonaspiceAr Nerd Font",
-      -- family = "MonaspiceXe Nerd Font",
-      -- family = "MonaspiceRa Nerd Font",
-      -- family = "MonaspiceKr Nerd Font",
-      weight = 'Medium',
-      harfbuzz_features = harfbuzz_features,
-    },
+  font = os_specific_font {
     {
       family = 'Monaspace Neon',
       -- family = "Monaspace Argon",
@@ -30,13 +21,22 @@ return {
       weight = 'Medium',
       harfbuzz_features = harfbuzz_features,
     },
+    {
+      family = 'MonaspiceNe Nerd Font',
+      -- family = "MonaspiceAr Nerd Font",
+      -- family = "MonaspiceXe Nerd Font",
+      -- family = "MonaspiceRa Nerd Font",
+      -- family = "MonaspiceKr Nerd Font",
+      weight = 'Medium',
+      harfbuzz_features = harfbuzz_features,
+    },
   },
 
   font_rules = {
     { -- Normal
       intensity = 'Normal',
       italic = false,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Neon',
           weight = 'Medium',
@@ -52,7 +52,7 @@ return {
     { -- Bold
       intensity = 'Bold',
       italic = false,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Argon',
           weight = 'ExtraBold',
@@ -68,7 +68,7 @@ return {
     { -- Half
       intensity = 'Half',
       italic = false,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Krypton',
           weight = 'Book',
@@ -84,7 +84,7 @@ return {
     { -- Normal italic
       intensity = 'Normal',
       italic = true,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Argon',
           weight = 'Regular',
@@ -102,7 +102,7 @@ return {
     { -- Bold italic
       intensity = 'Bold',
       italic = true,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Argon',
           weight = 'DemiBold',
@@ -120,7 +120,7 @@ return {
     { -- Half italic
       intensity = 'Half',
       italic = true,
-      font = font_with_fallback {
+      font = os_specific_font {
         {
           family = 'Monaspace Argon',
           weight = 'Thin',
