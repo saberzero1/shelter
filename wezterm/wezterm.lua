66c09b941651eae270d2606be00530f827cbb5c2
saberzero1 <github@emilebangma.com>
2025-04-02 12:00:38 +0200

    Updated font sizes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,47 +1,47 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
-wezterm.log_info 'The config was reloaded for this window!'
+wezterm.log_info("The config was reloaded for this window!")
 
 local function mergeTables(t1, t2)
-  for key, value in pairs(t2) do
-    t1[key] = value
-  end
+	for key, value in pairs(t2) do
+		t1[key] = value
+	end
 end
 
 local function basename(s)
-  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
 local config = {
-  front_end = 'WebGpu',
-  max_fps = 120,
+	front_end = "WebGpu",
+	max_fps = 120,
 
-  font_size = 12.0,
-  line_height = 1,
-  cell_width = 1,
+	font_size = 18.0,
+	line_height = 1,
+	cell_width = 1,
 
-  default_workspace = '~',
-  font = require('font').font,
-  font_rules = require('font').font_rules,
+	default_workspace = "~",
+	font = require("font").font,
+	font_rules = require("font").font_rules,
 
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-  hide_tab_bar_if_only_one_tab = true,
-  hide_mouse_cursor_when_typing = false,
-  inactive_pane_hsb = {
-    brightness = 0.9,
-  },
-  scrollback_lines = 5000,
-  audible_bell = 'Disabled',
-  enable_scroll_bar = true,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	hide_tab_bar_if_only_one_tab = true,
+	hide_mouse_cursor_when_typing = false,
+	inactive_pane_hsb = {
+		brightness = 0.9,
+	},
+	scrollback_lines = 5000,
+	audible_bell = "Disabled",
+	enable_scroll_bar = true,
 
-  status_update_interval = 1000,
-  xcursor_theme = 'Adwaita', -- fix cursor bug on gnome + wayland
+	status_update_interval = 1000,
+	xcursor_theme = "Adwaita", -- fix cursor bug on gnome + wayland
 }
 
 -- Actions shortcut
@@ -147,12 +147,12 @@ config.macos_window_background_blur = 50
 end]]
 --
 
-local colors = require 'colors'
+local colors = require("colors")
 mergeTables(config, colors)
 
-config.leader = { key = 'Space', mods = 'CTRL|SHIFT', timeout_milliseconds = 1000 }
-config.keys = require 'keybinds'
-config.mouse_bindings = require 'mousebinds'
+config.leader = { key = "Space", mods = "CTRL|SHIFT", timeout_milliseconds = 1000 }
+config.keys = require("keybinds")
+config.mouse_bindings = require("mousebinds")
 -- Actions
 --[[wezterm.on('update-right-status', function(window, pane)
   window:set_right_status(window:active_workspace())
@@ -234,25 +234,25 @@ modal.add_mode("Visual", require("visual_mode").key_table, require("visual_mode"
 }]]
 --
 
-local modal = wezterm.plugin.require 'https://github.com/MLFlexer/modal.wezterm'
+local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
 modal.apply_to_config(config)
 
-wezterm.on('modal.enter', function(name, window, pane)
-  modal.set_right_status(window, name)
-  modal.set_window_title(pane, name)
+wezterm.on("modal.enter", function(name, window, pane)
+	modal.set_right_status(window, name)
+	modal.set_window_title(pane, name)
 end)
 
-wezterm.on('modal.exit', function(name, window, pane)
-  local title = basename(window:active_workspace())
-  window:set_right_status(wezterm.format {
-    { Attribute = { Intensity = 'Bold' } },
-    { Foreground = { Color = colors.colors.ansi[5] } },
-    { Text = title .. '  ' },
-  })
-  modal.reset_window_title(pane)
+wezterm.on("modal.exit", function(name, window, pane)
+	local title = basename(window:active_workspace())
+	window:set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Foreground = { Color = colors.colors.ansi[5] } },
+		{ Text = title .. "  " },
+	}))
+	modal.reset_window_title(pane)
 end)
 
-local resurrect = wezterm.plugin.require 'https://github.com/MLFlexer/resurrect.wezterm'
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 resurrect.periodic_save()
 --[[resurrect.set_encryption({
 	enable = true,
@@ -261,72 +261,72 @@ resurrect.periodic_save()
 })]]
 --
 
-local workspace_switcher = wezterm.plugin.require 'https://github.com/MLFlexer/smart_workspace_switcher.wezterm'
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 workspace_switcher.apply_to_config(config)
 workspace_switcher.workspace_formatter = function(label)
-  return wezterm.format {
-    { Attribute = { Italic = true } },
-    { Foreground = { Color = colors.colors.ansi[3] } },
-    { Background = { Color = colors.colors.background } },
-    { Text = '󱂬 : ' .. label },
-  }
+	return wezterm.format({
+		{ Attribute = { Italic = true } },
+		{ Foreground = { Color = colors.colors.ansi[3] } },
+		{ Background = { Color = colors.colors.background } },
+		{ Text = "󱂬 : " .. label },
+	})
 end
 
-wezterm.on('smart_workspace_switcher.workspace_switcher.created', function(window, path, label)
-  window:gui_window():set_right_status(wezterm.format {
-    { Attribute = { Intensity = 'Bold' } },
-    { Foreground = { Color = colors.colors.ansi[5] } },
-    { Text = basename(path) .. '  ' },
-  })
-  local workspace_state = resurrect.workspace_state
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+	window:gui_window():set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Foreground = { Color = colors.colors.ansi[5] } },
+		{ Text = basename(path) .. "  " },
+	}))
+	local workspace_state = resurrect.workspace_state
 
-  workspace_state.restore_workspace(resurrect.load_state(label, 'workspace'), {
-    window = window,
-    relative = true,
-    restore_text = true,
-    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-  })
+	workspace_state.restore_workspace(resurrect.load_state(label, "workspace"), {
+		window = window,
+		relative = true,
+		restore_text = true,
+		on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+	})
 end)
 
-wezterm.on('smart_workspace_switcher.workspace_switcher.chosen', function(window, path, label)
-  window:gui_window():set_right_status(wezterm.format {
-    { Attribute = { Intensity = 'Bold' } },
-    { Foreground = { Color = colors.colors.ansi[5] } },
-    { Text = basename(path) .. '  ' },
-  })
+wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, path, label)
+	window:gui_window():set_right_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Foreground = { Color = colors.colors.ansi[5] } },
+		{ Text = basename(path) .. "  " },
+	}))
 end)
 
-wezterm.on('smart_workspace_switcher.workspace_switcher.selected', function(window, path, label)
-  local workspace_state = resurrect.workspace_state
-  resurrect.save_state(workspace_state.get_workspace_state())
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+	local workspace_state = resurrect.workspace_state
+	resurrect.save_state(workspace_state.get_workspace_state())
 end)
 
-local smart_splits = wezterm.plugin.require 'https://github.com/mrjones2014/smart-splits.nvim'
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 smart_splits.apply_to_config(config, {
-  direction_keys = { 'h', 'j', 'k', 'l' },
-  modifiers = {
-    move = 'CTRL',
-    resize = 'ALT',
-  },
+	direction_keys = { "h", "j", "k", "l" },
+	modifiers = {
+		move = "CTRL",
+		resize = "ALT",
+	},
 })
 
-wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-  local zoomed = ''
-  if tab.active_pane.is_zoomed then
-    zoomed = ' '
-  end
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+	local zoomed = ""
+	if tab.active_pane.is_zoomed then
+		zoomed = " "
+	end
 
-  local index = ''
-  if #tabs > 1 then
-    index = string.format('(%d/%d) ', tab.tab_index + 1, #tabs)
-  end
+	local index = ""
+	if #tabs > 1 then
+		index = string.format("(%d/%d) ", tab.tab_index + 1, #tabs)
+	end
 
-  return zoomed .. index .. tab.active_pane.title
+	return zoomed .. index .. tab.active_pane.title
 end)
 
-wezterm.on('gui-startup', function(cmd)
-  local _, _, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 -- and finally, return the configuration to wezterm
