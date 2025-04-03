128e7c50350689aa5cce7236208dd43b9e0a792a
saberzero1 <github@emilebangma.com>
2025-04-03 17:04:01 +0200

    Updated quotes

--- yabai/macos-setup.sh
+++ yabai/macos-setup.sh
@@ -17,16 +17,16 @@ fi
 # Check if /private/etc/sudoers.d/yabai exists
 if [ ! -f "/private/etc/sudoers.d/yabai" ]; then
   echo "/private/etc/sudoers.d/yabai does not exists. Creating it."
-  sudo touch /private/etc/sudoers.d/yabai
+  sudo touch "/private/etc/sudoers.d/yabai"
 fi
 
 # Construct the new sudoers entry
-SUDOERS_ENTRY="emile ALL=(root) NOPASSWD:$HASH "$YABAI_PATH" --load-sa"
+SUDOERS_ENTRY="emile ALL=(root) NOPASSWD:$HASH \"$YABAI_PATH\" --load-sa"
 
 # Write the netry to /private/etc/sudoers.d/yabai
-echo "$SUDOERS_ENTRY" | sudo tee /private/etc/sudoers.d/yabai >/dev/null
+echo "$SUDOERS_ENTRY" | sudo tee "/private/etc/sudoers.d/yabai" >/dev/null
 
 # Set the appropriate permissions for the sudoers file
-sudo chmod 0440 /private/etc/sudoers.d/yabai
+sudo chmod 0440 "/private/etc/sudoers.d/yabai"
 
 echo "Updated /private/etc/sudoers.d/yabai successfully."
